4ea5ae54d77c4c2c76c51799aaf65d68ec3e8d98
Karl Young <Karlyoung112@gmail.com>
2024-04-12 15:47:21 +1200

    Set up project with dependencies and ESLint

---
+++ client/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>E-Comm</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
