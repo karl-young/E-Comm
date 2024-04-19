183c1a2bc2dc23c88873b9d138ce7336a5f4b110
Karl Young <Karlyoung112@gmail.com>
2024-04-19 12:06:02 +1200

    Fixing db and routes

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -7,11 +7,5 @@ const env = (process.env.NODE_ENV || 'development') as Environment
 const connection = knex(config[env])
 
 export default connection
-export function getAllProducts() {
-  throw new Error('Function not implemented.')
-}
 
-export function getProductById(arg0: number) {
-  throw new Error('Function not implemented.')
-}
 
--- server/routes/products.ts
+++ server/routes/products.ts
@@ -1,12 +1,11 @@
 import express from 'express'
-import * as db from '../db/connection'
 import { getAllProducts, getProductById } from '../db/db'
 const router = express.Router()
 
 // GET /api/products - get all products
 router.get('/api/products', async (req, res) => {
   try {
-    const products = await db.getAllProducts()
+    const products = await getAllProducts()
 
     res.json(products)
   } catch (error: any) {
@@ -18,7 +17,7 @@ router.get('/api/products', async (req, res) => {
 // Get /api/products/:id - get product by id
 router.get('/api/products/:id', async (req, res) => {
   try {
-    const product = await db.getProductById(Number(req.params.id))
+    const product = await getProductById(Number(req.params.id))
     if (product === null) {
       res.status(404).send('Product not found')
     } else {
@@ -31,9 +30,20 @@ router.get('/api/products/:id', async (req, res) => {
 })
 
 // POST /api/products - create new product
+router.post('/api/products', async (req, res) => {
+  try {
+    const product = req.body
+    const newProduct = await createProduct(product)
+    res.json(newProduct)
+  } catch (error: any) {
+    res.status(500).send('Error creating product')
+    console.error(error.message)
+  }
+})
 
 // PUT /api/products/:id - update product by id
 
 // DELETE /api/products/:id - delete product by id
 
 export default router
+
