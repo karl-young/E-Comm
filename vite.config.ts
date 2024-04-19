2cb8ac82ecd0762dc733e5a43c06a2c52772d683
Karl Young <Karlyoung112@gmail.com>
2024-04-19 13:21:28 +1200

    Still fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
+  },
+  esbuild: {
+    // Enable ESM output
+    target: 'esnext',
   },
 })
