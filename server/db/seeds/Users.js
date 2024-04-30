335885f179dfe9eba2d07e82f9985059564c635f
Karl Young <Karlyoung112@gmail.com>
2024-04-30 14:06:49 +1200

    remembered how to make seeds

--- server/db/seeds/Users.js
+++ server/db/seeds/Users.js
@@ -6,8 +6,29 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('Users').del()
   await knex('Users').insert([
-    { id: 1, name: 'Dave', email: 'Dave12@example.com', password: 'password' },
-    { id: 2, name: 'Sarah', email: 'Sarah53@example.com', password: '1234987' },
-    { id: 3, name: 'Karl', email: 'Karl71@example.com', password: 'hnfgiua8' },
+    {
+      id: 1,
+      name: 'Dave',
+      email: 'Dave12@example.com',
+      password: 'password',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      id: 2,
+      name: 'Sarah',
+      email: 'Sarah53@example.com',
+      password: '1234987',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      id: 3,
+      name: 'Karl',
+      email: 'Karl71@example.com',
+      password: 'hnfgiua8',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
   ])
 }
