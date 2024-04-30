b25cc97031f0edb5435729b510fd1eac76e77c96
Karl Young <Karlyoung112@gmail.com>
2024-04-30 13:40:16 +1200

    Adding seeds

---
+++ server/db/seeds/Users.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('Users').del()
+  await knex('Users').insert([
+    { id: 1, name: 'Dave', email: 'Dave12@example.com', password: 'password' },
+    { id: 2, name: 'Sarah', email: 'Sarah53@example.com', password: '1234987' },
+    { id: 3, name: 'Karl', email: 'Karl71@example.com', password: 'hnfgiua8' },
+  ])
+}
